f8fe328f606f8317e735d5c1ef197871cd5d7c73
Maxence Caille <caille.maxence@outlook.fr>
2021-11-28 13:37:14 +0100

    Programme java 1 task 2 terminé

--- src/main/java/emse/TaskTwoProgramOne.java
+++ src/main/java/emse/TaskTwoProgramOne.java
@@ -16,14 +16,14 @@ public class TaskTwoProgramOne {
             String nameBucket = "bucket3688480748";
             String nameFile = "\"new_user_credentials.csv\"";
             String pathFile = "C:\\Users\\caill\\Desktop\\Cours Mines\\Majeure\\cours 2a info\\Cloud";
-/*
+
             S3ControllerCreate.main(new String[]{nameBucket});
 
             sleep(5000); // We add some delay in order to do not have any error because of the time it takes to create the bucket
 
             S3ControllerPutObject.main(new String[]{nameBucket, nameFile,pathFile });
 
-            sleep(5000); // We add some delay in order to do not have any error because of the time it takes to add the object */
+            sleep(5000); // We add some delay in order to do not have any error because of the time it takes to add the object
 
             SqsClient sqsClient = SqsClient.builder()
                     .build();
@@ -32,12 +32,11 @@ public class TaskTwoProgramOne {
 
             CreateSQS.createQueue(sqsClient, queueName);
 
-            sleep(5000); // We add some delay in order to do not have any error because of the time it takes to create a queue */
+            sleep(5000); // We add some delay in order to do not have any error because of the time it takes to create a queue
 
             SendMessageSQS.sendMessages(sqsClient,queueName,nameBucket,nameFile);
 
         }
-
 
 
         catch(InterruptedException ex)
